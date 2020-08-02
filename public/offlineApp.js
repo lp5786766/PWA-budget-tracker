475c15b5784f91860c49f0f6446990dc012fb797
Luba Pecheneva <lp5786766@gmail.com>
2020-08-02 13:10:31 -0400

    edited the Readme

---
+++ public/offlineApp.js
@@ -0,0 +1,12 @@
+if (navigator.onLine) {
+    console.log('online');
+  } else {
+    console.log('offline');
+  }
+
+
+
+
+// window.addEventListener('offline', function(e) { alert('offline'); });
+
+// window.addEventListener('online', function(e) { alert('online'); });
