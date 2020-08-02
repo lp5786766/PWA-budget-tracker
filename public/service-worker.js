e8f3915dd5181ffa9631a354fad02422fc3d7fe3
Luba Pecheneva <lp5786766@gmail.com>
2020-08-02 12:42:38 -0400

    the app is downloadable

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,14 @@
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/style.css',
+  '/styles.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
   '/index.js',
   '/manifest.webmanifest',
-  '/favicon.ico',
 ];
 
 const CACHE_NAME = 'static-cache-v2';
-
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 self.addEventListener('install', function (evt) {
